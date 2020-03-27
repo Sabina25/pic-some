0d49fad0bfea131c996ca9056946e00e2073adbf
Сабина Кадырова <sabina@Sabinas-MacBook-Pro.local>
2020-03-27 20:06:55 +0200

    er

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Backet from "../images/icon/hotpng.com.png"
+
 
 function Header() {
     return (
         <header>
             <h2>Pic Some</h2>
-            <Backet />
+            
         </header>
     )
 }
