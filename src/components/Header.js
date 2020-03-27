5de13188ac0eed501a7961d2fafd62309a2ab2e0
Сабина Кадырова <sabina@Sabinas-MacBook-Pro.local>
2020-03-27 19:54:11 +0200

    New project

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Backet from "../images/icon/hotpng.com.png"
+
+function Header() {
+    return (
+        <header>
+            <h2>Pic Some</h2>
+            <Backet />
+        </header>
+    )
+}
+
+export default Header
